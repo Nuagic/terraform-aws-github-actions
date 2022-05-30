ab5da1fca983e0057e309ea796b14ec2fa5664c0
Cyril Feraudet <cyril@feraudet.com>
2022-05-30 14:41:36 +0200

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,14 @@ output "aws_iam_user" {
   value       = var.create_aksk ? aws_iam_user.this[0].name : null
 }
 
+output "aws_access_key_id" {
+  description = "AWS_ACCESS_KEY_ID"
+  value       = var.create_aksk ? aws_iam_access_key.this[0].id : null
+}
+
+output "aws_secret_access_key" {
+  description = "AWS_SECRET_ACCESS_KEY"
+  value       = var.create_aksk ? aws_iam_access_key.this[0].secret : null
+  sensitive   = true
+}
+
